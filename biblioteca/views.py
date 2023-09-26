e24751a876a71a7fac897a89e0659622d82c1c54
rock051 <fernando.roque@estudante.ufla.br>
2023-09-26 10:55:40 -0300

    Criando o primeiro Template no Django

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def principal(request):
-    return HttpResponse("Olá Mundo! - Portal Biblioteca")
+    template = loader.get_template('principal.html')
+    return HttpResponse(template.render())
